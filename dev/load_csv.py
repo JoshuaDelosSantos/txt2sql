b925a05d4ca1980e66f00fd8a34cbdc25f1d80d7
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-13 19:41:35 +1100

    Add initial functions

--- dev/load_csv.py
+++ dev/load_csv.py
@@ -7,7 +7,18 @@ import sys
 from pathlib import Path
 import psycopg2
 from dotenv import load_dotenv
+from db import get_connection
 
 ROOT_DIR = Path(__file__).parent.parent
 load_dotenv(ROOT_DIR / ".env")
 
+CSV_DIR = ROOT_DIR / "dev" / "csv_data"
+
+def main():
+    pass
+
+def drop_all(current):
+    pass
+
+def copy_table(current, table_name):
+    pass
