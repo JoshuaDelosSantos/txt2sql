753756ae88c57cdfbbe3979afb34869911b0459e
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-13 19:38:23 +1100

    Add initial imports

--- dev/load_csv.py
+++ dev/load_csv.py
@@ -1,3 +1,13 @@
 """
 Load mock csv data into Postgres database.
 """
+
+import os
+import sys
+from pathlib import Path
+import psycopg2
+from dotenv import load_dotenv
+
+ROOT_DIR = Path(__file__).parent.parent
+load_dotenv(ROOT_DIR / ".env")
+
