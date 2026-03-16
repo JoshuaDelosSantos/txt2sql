46add309a0d3d9a7577c50f0ee07a81b2a4b52ce
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-16 18:43:36 +1100

    Initial commit

---
+++ tools/schema.py
@@ -0,0 +1,5 @@
+"""
+Functions that can be used to extract the database schema, such as table names, column names, and data types. 
+This can be used to help generate SQL queries based on natural language input.
+"""
+
