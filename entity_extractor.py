6f7ef77f46fcf6de14c3584afc813b0395edf57a
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-17 19:20:48 +1100

    Add initial imports

--- entity_extractor.py
+++ entity_extractor.py
@@ -2,4 +2,10 @@
 Connect to a DB and extract entities from text query.
 """
 
+import logging
+from lanngchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from agents.results import EntityResult
+from config import settings
 
