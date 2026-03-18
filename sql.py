757e81a0954ab9b7d407a52d3f29e668b31bca75
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2026-03-18 18:48:57 +1100

    Initial imports

--- sql.py
+++ sql.py
@@ -2,4 +2,18 @@
 This file will return the SQL statements needed to create the database schema.
 """
 
+import logging
+import re
 
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+
+from results import SQLResult
+from config import settings
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s: %(message)s',
+    force=True,
+)
+logger = logging.getLogger(__name__)
